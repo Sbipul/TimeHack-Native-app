130b701d66cdc31c20639b1e7675e10fe65e9c75
bipul <kumarroy.kb.kb@gmail.com>
2022-04-04 00:01:42 +0600

    first

--- App.js
+++ App.js
@@ -1,69 +1,98 @@
 import React from "react";
-import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useHistory } from "react-router-native";
 import { NativeRouter, Link, Route } from "react-router-native";
 import DetailsScreen from "./DetailsScreen";
 import Form from "./Form";
 import Home from "./Home";
 const ScreenHeight = Dimensions.get("window").height;
-const ScreenWidth = Dimensions.get('window').width;
-
+const ScreenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 55,
-    position:'relative',
-    zIndex:2
+    position: "relative",
+    zIndex: 2,
   },
   cart: {
     width: 45,
-    height: 45
+    height: 45,
+    borderRadius: 22,
+    backgroundColor: "#DDDDDD",
   },
-  bg:{
-    backgroundColor:'#eaebed',
-    marginTop:25,
-    paddingHorizontal:40,
-    paddingVertical:10,
-    flexDirection: 'row',
-    position:'absolute',
-    width:ScreenWidth,
-    overflow:'hidden',
-    zIndex:5,
-    alignItems:'center',
-    justifyContent:'space-between',
-    bottom:-30,
-    left:0,
-    borderColor:'grey'
+  bg: {
+    backgroundColor: "#eaebed",
+    marginTop: 25,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    flexDirection: "row",
+    position: "absolute",
+    width: ScreenWidth,
+    overflow: "hidden",
+    zIndex: 5,
+    alignItems: "center",
+    justifyContent: "space-between",
+    bottom: -30,
+    left: 0,
+    borderColor: "grey",
   },
-  iconImage:{
-    height:50,
-    width:50
+  iconImage: {
+    height: 50,
+    width: 50,
   },
-  textColor:{
-    color:'white'
+  textColor: {
+    color: "white",
   },
-  totalHeight:{
-    height:ScreenHeight,
-    position:'relative',
-    zIndex:1
+  totalHeight: {
+    height: ScreenHeight,
+    position: "relative",
+    zIndex: 1,
   },
-
 });
 export default function App() {
+  const history = useHistory();
   return (
     <NativeRouter>
       <View style={styles.totalHeight}>
         <View style={styles.bg}>
-            <Link to="/">
-              <Image style={styles.cart} source={{uri:"https://dixonpestcontrol.com/wp-content/uploads/2017/10/house-icon.png"}}/>
+          <TouchableOpacity style={styles.cart}>
+            <Link style={styles.cart} to="/">
+              <Image
+                style={styles.cart}
+                source={{
+                  uri: "https://dixonpestcontrol.com/wp-content/uploads/2017/10/house-icon.png",
+                }}
+              />
             </Link>
-            <Link to="/form">
-              <Image style={styles.cart} source={{uri:"https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-add-icon.png"}}/>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Link style={styles.cart} to="/form">
+              <Image
+                style={styles.cart}
+                source={{
+                  uri: "https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/sign-add-icon.png",
+                }}
+              />
             </Link>
-            <Link to="/details">
-              <Image style={styles.cart} source={{uri:"https://accessories.edunet.com.au/Content/images/OrderSearch.png"}}/>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.cart}>
+            <Link to="/details" style={styles.cart}>
+              <Image
+                style={styles.cart}
+                source={{
+                  uri: "https://accessories.edunet.com.au/Content/images/OrderSearch.png",
+                }}
+              />
             </Link>
+          </TouchableOpacity>
         </View>
-        
+
         <View style={styles.container}>
           <Route exact path="/" component={Home} />
           <Route path="/details" component={DetailsScreen} />
@@ -73,4 +102,3 @@ export default function App() {
     </NativeRouter>
   );
 }
-
--- Form.js
+++ Form.js
@@ -1,17 +1,91 @@
-import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Dimensions,
+  TextInput,
+  ImageBackground
+} from "react-native";
+import React, { useState } from "react";
+import { Slide } from "native-base";
+
+const { width } = Dimensions.get("window");
+const height = width * 0.8;
+
 const Form = () => {
+  const images = [
+    "https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+    "https://media.istockphoto.com/photos/beautiful-sunrise-landscape-in-high-mountain-road-picture-id615619640?k=20&m=615619640&s=170667a&w=0&h=eOugsz8OZpAMmgzNAOr9JOjphUE00PE23d5RRu4N1GU=",
+    "https://images.unsplash.com/photo-1612151855475-877969f4a6cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aGQlMjBpbWFnZXxlbnwwfHwwfHw%3D&w=1000&q=80",
+    "https://live.staticflickr.com/7324/10494525026_364e78d36c_b.jpg",
+  ];
+  const [active, setActive] = useState(0);
+
+  const change = ({ nativeEvent }) => {
+    const slide = Math.ceil(
+      nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width
+    );
+    if (slide !== active) {
+      setActive(active);
+    }
+  };
   return (
     <View>
-      <Text style={styles.textColor}>My name is bipul chandro roy.I love react native</Text>
+      <View style={styles.search}>
+      <TextInput style={styles.input}/>
+      </View>
+      <ScrollView
+        onScroll={change}
+        pagingEnabled
+        horizontal
+        style={styles.scrollViewDiv}
+        showsHorizontalScrollIndicator={false}
+      >
+        {images?.map((image, index) => (
+          <View style={styles.container} key={index}>
+            <Image
+              source={{
+                uri: image,
+              }}
+              style={styles.image}
+            ></Image>
+            <Text style={styles.textColor}>
+              My name is bipul chandro roy.I love react native
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.indicator}>
+        {images?.map((i, k) => (
+          <Text
+            key={k}
+            style={k === active ? styles.activeColor : styles.commonColor}
+          >
+            ⬤
+          </Text>
+        ))}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
 
 const styles = StyleSheet.create({
-    textColor:{
-        color:'black'
-    }
-})
+  textColor: { color: "black" },
+  container: { marginTop: 0, width, height },
+  image: { height, width, resizeMode: "cover" },
+  scrollViewDiv: { height, width },
+  indicator: {
+    flexDirection: "row",
+    bottom: 0,
+    position: "absolute",
+    alignSelf: "center",
+  },
+  commonColor: { color: "white", margin: 3 },
+  activeColor: { color: "red", margin: 3 },
+  search:{height:50,backgroundColor:'#fffddd',width:'100%'},
+  input:{height:'100%',width:'100%',padding:10,backgroundColor:'red'}
+});
